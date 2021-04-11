94c0756666a1f0da0a6652e149a12647d18f2252
hothanhnguyenpro <79977840+hothanhnguyenpro@users.noreply.github.com>
2021-04-11 08:38:44 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+
+const config = require('./src/data/config.json');
+
+const debug = config.debug;
+if(!debug) var tracer = require('dd-trace').init();
+
+if(debug) var auth = require('../tokens/scuttester-auth.json');
+else var auth = require('../tokens/owo-auth.json');
+
+const request = require('./utils/request.js');
+// Eris-Sharder
+const Sharder = require('eris-sharder').Master;
+var result,shards,firstShardID,lastShardID;
+
+// Helper files
+if(require('cluster').isMaster){
+	const global = require('./utils/global.js');
+	const RamCheck = new (require('./utils/ramCheck.js'))(global);
+}
+
+const totalShards = 10;
+
+(async () => {
+	try{
+		//determine how many shards we will need for this manager
+		if (!debug&&require('cluster').isMaster){
+			result = await request.fetchInit();
+			console.log(result);
+			shards = parseInt(result["shards"]);
+			firstShardID = parseInt(result["firstShardID"]);
+			lastShardID = parseInt(result["lastShardID"]);
+		}
+		// How many clusters we will have
+		var clusters = Math.ceil(shards/totalShards);
+		if(debug){
+			shards = 2;
+			firstShardID = 0;
+			lastShardID = 1;
+			clusters = 1
+		}
+
+		console.log("Creating shards "+firstShardID+"~"+lastShardID+" out of "+shards+" total shards!");
+
+		// Start sharder
+		const sharder = new Sharder("Bot "+auth.token, config.sharder.path, {
+			name: config.sharder.name,
+			clientOptions: config.eris.clientOptions,
+			debug:true,
+			shards,clusters,
+			firstShardID,
+			lastShardID,
+		});
+
+	}catch(e){
+		console.error("Failed to start eris sharder");
+		console.error(e);
+	}
+})();
